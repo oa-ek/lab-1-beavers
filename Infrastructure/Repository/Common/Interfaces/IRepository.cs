6eb9d4d123c0b0284b15675db87a1c7edc5243be
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-15 19:48:35 +0300

    Created repository for the database

---
+++ Infrastructure/Repository/Common/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.Repository.Common.Interfaces;
+
+public interface IRepository<TEntity> 
+    where TEntity : class
+{
+    Task<IEnumerable<TEntity>> GetAllEntitiesAsync();
+
+    Task<TEntity> GetSingleEntityBySpecificationAsync(Func<TEntity, bool> predicate);
+
+    Task AddNewEntityAsync(TEntity entity);
+
+    void UpdateExistingEntity(TEntity updatedEntity);
+
+    void RemoveExistingEntity(TEntity removedEntity);
+}
---
+++ Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Data.Contexts;
+using Infrastructure.Repository.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository;
+
+public class GenericRepository<TEntity> : IRepository<TEntity>
+    where TEntity : class
+{
+    protected GenericRepository(ApplicationContext dbContext) => 
+        Context = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    private ApplicationContext Context { get; init; }
+
+    public async Task<IEnumerable<TEntity>> GetAllEntitiesAsync() =>
+        await Context.Set<TEntity>().ToListAsync();
+
+    public async Task<TEntity> GetSingleEntityBySpecificationAsync(Func<TEntity, bool> predicate) =>
+        (await Context.Set<TEntity>().SingleOrDefaultAsync())!;
+
+    public async Task AddNewEntityAsync(TEntity entity)
+    {
+        await Context.Set<TEntity>().AddAsync(entity);
+        await Context.SaveChangesAsync();
+    }
+    
+    public void UpdateExistingEntity(TEntity updatedEntity)
+    {
+        Context.Set<TEntity>().Update(updatedEntity);
+        Context.SaveChanges();
+    }
+
+    public virtual void RemoveExistingEntity(TEntity removedEntity)
+    {
+        Context.Set<TEntity>().Remove(removedEntity);
+        Context.SaveChanges();
+    }
+}
