8fc6dec4b9f8a183b5e468ca2c6e07d509a31454
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-19 20:32:01 +0300

    Added validation for user controller

--- BaverGame/DTOs/ValidationRelated/RegexPatterns.cs
+++ BaverGame/DTOs/ValidationRelated/RegexPatterns.cs
@@ -4,4 +4,5 @@ public static class RegexPatterns
 {
     public const string GuidPattern = @"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
     public const string UrlPattern = @"^(https?|ftp)://[^\s/$.?#].[^\s]*$";
+    public const string EmailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
 }
