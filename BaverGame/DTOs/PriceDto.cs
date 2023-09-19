7e562c6afd17df2e5d35457b0cc1c737ec1afd38
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-19 13:50:41 +0300

    Created static classes with required attributes

--- BaverGame/DTOs/PriceDto.cs
+++ BaverGame/DTOs/PriceDto.cs
@@ -1,32 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using BaverGame.DTOs.ValidationRelated;
 
 namespace BaverGame.DTOs;
 
 public sealed class PriceDto
 {
-    [StringLength(36, MinimumLength = 36, ErrorMessage = "PriceId must be a 36-character GUID.")]
-    [RegularExpression(@"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", 
-        ErrorMessage = "GameId is not in a valid GUID format.")]
+    [Required(ErrorMessage = ValidationMessages.RequiredField)]
+    [StringLength(36, MinimumLength = 36, ErrorMessage = ValidationMessages.InvalidGuidFormat)]
+    [RegularExpression(RegexPatterns.GuidPattern, ErrorMessage = ValidationMessages.InvalidGuidFormat)]
     public string PriceId { get; set; }
-    
-    [Required(ErrorMessage = "GameId is required.")]
-    [StringLength(36, MinimumLength = 36, ErrorMessage = "GameId must be a 36-character GUID.")]
-    [RegularExpression(@"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", 
-        ErrorMessage = "GameId is not in a valid GUID format.")]
+
+    [Required(ErrorMessage = ValidationMessages.RequiredField)]
+    [StringLength(36, MinimumLength = 36, ErrorMessage = ValidationMessages.InvalidGuidFormat)]
+    [RegularExpression(RegexPatterns.GuidPattern, ErrorMessage = ValidationMessages.InvalidGuidFormat)]
     public string GameId { get; set; }
 
-    [Required(ErrorMessage = "StoreId is required.")]
-    [StringLength(36, MinimumLength = 36, ErrorMessage = "StoreId must be a 36-character GUID.")]
-    [RegularExpression(@"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4]-[0-9a-fA-F]{4}-[0-9a-fA-F]{4]-[0-9a-fA-F]{12}$",
-        ErrorMessage = "StoreId is not in a valid GUID format.")]
+    [Required(ErrorMessage = ValidationMessages.RequiredField)]
+    [StringLength(36, MinimumLength = 36, ErrorMessage = ValidationMessages.InvalidGuidFormat)]
+    [RegularExpression(RegexPatterns.GuidPattern, ErrorMessage = ValidationMessages.InvalidGuidFormat)]
     public string StoreId { get; set; }
 
-    [Required(ErrorMessage = "PriceValue is required.")]
-    [Range(0.01, double.MaxValue, ErrorMessage = "PriceValue must be greater than 0.")]
+    [Required(ErrorMessage = ValidationMessages.RequiredField)]
+    [Range(0.01, double.MaxValue, ErrorMessage = ValidationMessages.InvalidNumericValue)]
     public decimal PriceValue { get; set; }
 
-    [Required(ErrorMessage = "PriceUrl is required.")]
-    [RegularExpression(@"^(https?|ftp)://[^\s/$.?#].[^\s]*$", 
-        ErrorMessage = "Invalid URL format.")]
+    [Required(ErrorMessage = ValidationMessages.RequiredField)]
+    [RegularExpression(RegexPatterns.UrlPattern, ErrorMessage = ValidationMessages.InvalidUrlFormat)]
     public string PriceUrl { get; set; }
 }
