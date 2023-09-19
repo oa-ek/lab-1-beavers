8b7ae7ba9f6ce84282acd2942deb311967e3701d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-19 13:52:46 +0300

    Changed message of InvalidNumericValue in ValidationMessages.cs

--- BaverGame/DTOs/ValidationRelated/ValidationMessages.cs
+++ BaverGame/DTOs/ValidationRelated/ValidationMessages.cs
@@ -5,5 +5,5 @@ public static class ValidationMessages
     public const string RequiredField = "{0} is required.";
     public const string InvalidGuidFormat = "{0} must be a 36-character GUID.";
     public const string InvalidUrlFormat = "Invalid URL format.";
-    public const string InvalidNumericValue = "Invalid numeric value.";
+    public const string InvalidNumericValue = "Invalid {0} numeric value.";
 }
