2b9076c6615b2b7bf1363117266bd6ff2bfb4ebb
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-10-29 22:38:26 +0200

    Updated UserDto.cs

--- BaverGame/DTOs/UserDto.cs
+++ BaverGame/DTOs/UserDto.cs
@@ -11,6 +11,11 @@ public sealed class UserDto
     public string UserId { get; set; }
     
     [Required(ErrorMessage = ValidationMessages.RequiredField)]
+    [StringLength(36, MinimumLength = 36, ErrorMessage = ValidationMessages.InvalidGuidFormat)]
+    [RegularExpression(RegexPatterns.GuidPattern, ErrorMessage = ValidationMessages.InvalidGuidFormat)]
+    public string RoleId { get; set; }
+    
+    [Required(ErrorMessage = ValidationMessages.RequiredField)]
     [StringLength(255, MinimumLength = 3, ErrorMessage = ValidationMessages.InvalidValue)]
     public string Username { get; set; }
     
