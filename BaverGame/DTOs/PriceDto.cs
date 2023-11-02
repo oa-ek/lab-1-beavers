b20666536b455f077f29c60c95a14c8eb2812c93
Maxim Moskalyk <ictic.step@gmail.com>
2023-11-02 11:25:29 +0200

    Price creating and modified changes.

--- BaverGame/DTOs/PriceDto.cs
+++ BaverGame/DTOs/PriceDto.cs
@@ -21,7 +21,7 @@ public sealed class PriceDto
     public string StoreId { get; set; }
 
     [Required(ErrorMessage = ValidationMessages.RequiredField)]
-    [Range(0.01, double.MaxValue, ErrorMessage = ValidationMessages.InvalidNumericValue)]
+    [Range(0, double.MaxValue, ErrorMessage = ValidationMessages.InvalidNumericValue)]
     public decimal PriceValue { get; set; }
 
     [Required(ErrorMessage = ValidationMessages.RequiredField)]
