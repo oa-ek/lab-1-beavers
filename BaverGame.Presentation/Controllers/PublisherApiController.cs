99ba7449171ee09e0e1a099c989de18e3f54cf16
Maxim Moskalyk <ictic.step@gmail.com>
2023-12-06 21:36:03 +0200

    Controllers updated.

--- BaverGame.Presentation/Controllers/PublisherApiController.cs
+++ BaverGame.Presentation/Controllers/PublisherApiController.cs
@@ -1,35 +1,84 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BaverGame.Domain.Contracts;
 using BaverGame.Domain.Entities;
-using Microsoft.AspNetCore.Http;
+using BaverGame.Application.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace BaverGame.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class PublisherApiController : ControllerBase
     {
         private readonly IRepository<Publisher> _publishersRepository;
-    
+
         public PublisherApiController(IRepository<Publisher> publishersRepository)
         {
             _publishersRepository = publishersRepository;
         }
-        
+
+        // GET: api/Publisher
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             return Ok(await _publishersRepository.GetAllEntitiesAsync());
         }
-        
-        [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] Guid id)
+
+        // GET: api/Publisher/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
         {
-            return Ok(await _publishersRepository.GetEntityByIdAsync(id));
+            var publisher = await _publishersRepository.GetEntityByIdAsync(id);
+            if (publisher == null)
+                return NotFound();
+
+            return Ok(publisher);
+        }
+
+        // POST: api/Publisher
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] PublisherDto publisherDto)
+        {
+            if (string.IsNullOrWhiteSpace(publisherDto.PublisherName))
+                return BadRequest("Publisher name is required.");
+
+            var publisher = new Publisher
+            {
+                PublisherName = publisherDto.PublisherName
+            };
+            await _publishersRepository.AddNewEntityAsync(publisher);
+
+            return CreatedAtAction(nameof(Get), new { id = publisher.PublisherId }, publisher);
+        }
+
+        // PUT: api/Publisher/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] PublisherDto publisherDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var publisher = await _publishersRepository.GetEntityByIdAsync(id);
+            if (publisher == null)
+                return NotFound();
+
+            publisher.PublisherName = publisherDto.PublisherName;
+            _publishersRepository.UpdateExistingEntity(publisher);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Publisher/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var publisher = await _publishersRepository.GetEntityByIdAsync(id);
+            if (publisher == null)
+                return NotFound();
+
+            _publishersRepository.RemoveExistingEntity(publisher);
+            return NoContent();
         }
     }
 }
--- BaverGame.Presentation/Controllers/TagApiController.cs
+++ BaverGame.Presentation/Controllers/TagApiController.cs
@@ -1,35 +1,84 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BaverGame.Domain.Contracts;
 using BaverGame.Domain.Entities;
-using Microsoft.AspNetCore.Http;
+using BaverGame.Application.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace BaverGame.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class TagApiController : ControllerBase
     {
         private readonly IRepository<Tag> _tagsRepository;
-    
+
         public TagApiController(IRepository<Tag> tagsRepository)
         {
             _tagsRepository = tagsRepository;
         }
-        
+
+        // GET: api/Tag
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             return Ok(await _tagsRepository.GetAllEntitiesAsync());
         }
-        
-        [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] Guid id)
+
+        // GET: api/Tag/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
         {
-            return Ok(await _tagsRepository.GetEntityByIdAsync(id));
+            var tag = await _tagsRepository.GetEntityByIdAsync(id);
+            if (tag == null)
+                return NotFound();
+
+            return Ok(tag);
+        }
+
+        // POST: api/Tag
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TagDto tagDto)
+        {
+            if (string.IsNullOrWhiteSpace(tagDto.TagName))
+                return BadRequest("Tag name is required.");
+
+            var tag = new Tag
+            {
+                TagName = tagDto.TagName
+            };
+            await _tagsRepository.AddNewEntityAsync(tag);
+
+            return CreatedAtAction(nameof(Get), new { id = tag.TagId }, tag);
+        }
+
+        // PUT: api/Tag/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] TagDto tagDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var tag = await _tagsRepository.GetEntityByIdAsync(id);
+            if (tag == null)
+                return NotFound();
+
+            tag.TagName = tagDto.TagName;
+            _tagsRepository.UpdateExistingEntity(tag);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Tag/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var tag = await _tagsRepository.GetEntityByIdAsync(id);
+            if (tag == null)
+                return NotFound();
+
+            _tagsRepository.RemoveExistingEntity(tag);
+            return NoContent();
         }
     }
 }
